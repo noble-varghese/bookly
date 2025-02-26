7e23d51319d583b8c159799705cc6619a04a0214
Noble Varghese <noblevarghese@Nobles-MacBook-Pro.local>
2025-02-26 09:52:39 +0530

    feat: updated the build commands

--- packages/graphql-schema/index.ts
+++ packages/graphql-schema/index.ts
@@ -1,6 +1,7 @@
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import path from 'path';
+export * from './src/types/generated';
 
 // Load and merge schema files
 const typesArray = loadFilesSync(path.join(__dirname, './src/schema'), {
@@ -8,4 +9,3 @@ const typesArray = loadFilesSync(path.join(__dirname, './src/schema'), {
 });
 
 export const schema = mergeTypeDefs(typesArray);
-export * from './src/types/generated';
