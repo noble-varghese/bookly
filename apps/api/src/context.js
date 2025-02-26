f14da08e1dcf65c82c31d99c111b9c1df4a6e862
Noble Varghese <noblevarghese@Nobles-MacBook-Pro.local>
2025-02-26 09:58:04 +0530

    chore: remove unwanted files

--- apps/api/src/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContext = void 0;
-// src/context.ts
-const dataloaders_1 = require("./dataloaders");
-const createContext = () => ({
-    loaders: (0, dataloaders_1.createLoaders)()
-});
-exports.createContext = createContext;
--- apps/api/src/resolvers/user/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userResolvers = void 0;
-const mutations_1 = require("./mutations");
-const queries_1 = require("./queries");
-exports.userResolvers = {
-    Query: queries_1.queries,
-    Mutation: mutations_1.mutations
-};
